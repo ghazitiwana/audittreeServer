f7a5ba89bebcbaf7e97182b1977f5e0c9eae5529
ghazitiwana <35327403+ghazitiwana@users.noreply.github.com>
2021-05-28 22:23:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask_restful import reqparse
+from flask import Flask, jsonify, request
+import pandas as pd
+from statsmodels.tsa.ar_model import AutoReg
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "hey"
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+  data = pd.read_csv('items.csv')
+  dataT = data.T
+  item1 = dataT.iloc[1:,1]
+  # split dataset
+  X = item1.values
+  train, test = X[1:len(X)-7], X[len(X)-7:]
+  # train autoregression
+  model = AutoReg(train, lags=14)
+  model_fit = model.fit()
+  # make predictions
+  predictions = model_fit.predict(start=len(train), end=len(train)+len(test)-1, dynamic=False)
+  print(predictions)
+  return predictions
+
+if __name__ == '__main__':
+    app.run(debug=True)
