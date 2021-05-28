d43d2d48ce355205dd775d27bbd756235da1c3c0
ghazitiwana <35327403+ghazitiwana@users.noreply.github.com>
2021-05-28 23:03:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ def prediction():
   dataT = data.T
   item1 = dataT.iloc[1:,1]
   # split dataset
-  X = item1.values
-  train, test = X[1:len(X)-7], X[len(X)-7:]
+  #X = item1.values
+  #train, test = X[1:len(X)-7], X[len(X)-7:]
   # train autoregression
-  model = AutoReg(train, lags=14)
-  model_fit = model.fit()
+  #model = AutoReg(train, lags=14)
+  #model_fit = model.fit()
   # make predictions
-  predictions = model_fit.predict(start=len(train), end=len(train)+len(test)-1, dynamic=False)
-  print(predictions)
-  return predictions
+  #predictions = model_fit.predict(start=len(train), end=len(train)+len(test)-1, dynamic=False)
+  #print(predictions)
+  return "woohoo"
 
 if __name__ == '__main__':
     app.run(debug=True)
